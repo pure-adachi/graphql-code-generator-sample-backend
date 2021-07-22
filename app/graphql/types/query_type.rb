e01aca51c231cfdd2a357535688fffa6b48de68b
adachi <takuya_adachi@pure-system.com>
2021-07-22 23:18:46 +0900

    gem version up

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
+    add_field GraphQL::Types::Relay::NodeField
+    add_field GraphQL::Types::Relay::NodesField
+
     field :todos, resolver: Resolvers::QueryType::TodosResolver
   end
 end
--- app/graphql/types/todo_type.rb
+++ app/graphql/types/todo_type.rb
@@ -2,10 +2,11 @@
 
 module Types
   class TodoType < Types::BaseObject
-    implements GraphQL::Relay::Node.interface
+    implements GraphQL::Types::Relay::Node
     implements Types::ValidationErrorInterface
 
     global_id_field :id
     field :name, String, null: true
+    field :created_at, GraphQL::Types::ISO8601Date, null: true
   end
 end
